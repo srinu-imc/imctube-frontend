6cc23c065a6720f382f6ba60479845967a192a9a
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-01-05 21:57:33 -0800

    Version '0' for ui

---
+++ js/imctube.js
@@ -0,0 +1,15 @@
+(function() {
+  var app = angular.module('imctubeApp', ['movie-directives']);
+
+  app.controller('MovieController', ['$http', function($http) {
+    var imctube = this;
+    imctube.movie = {};
+    imctube.currentClip = {};
+
+    $http.get('resources/data/movies.json').success(function(data) {
+      imctube.movie = data;
+      console.log(imctube.movie);
+    });
+  }]);
+})();
+
---
+++ js/movies.js
@@ -0,0 +1,71 @@
+(function() {
+  var app = angular.module('movie-directives', ['youtube-embed']);
+
+  // Youtube player directive which controls the youtube functions
+  app.directive("youtubePlayer", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "view/youtube-player.html",
+      controller: function() {
+        this.playVideo = function(player) {
+          player.playVideo();
+        };
+
+        this.pauseVideo = function(player) {
+          player.pauseVideo();
+        };
+
+        this.playFromMovieStart = function(player) {
+          player.seekTo(0, true);
+        };
+
+        this.backward = function(player) {
+          player.seekTo(player.getCurrentTime() - 10, true);
+        }
+
+        this.forward = function(player) {
+          player.seekTo(player.getCurrentTime() + 10, true);
+        }
+
+        this.playFromClipStart = function(player, clip) {
+          player.seekTo(clip.startTime, true);
+        };
+      },
+      controllerAs: 'ytPlayer'
+    };  
+  });
+
+  app.directive("clipData", function() {
+    return {
+      restrict: 'E',
+    templateUrl: 'view/clip-data-main.html',
+    controller: function() {
+
+      angular.element(document).ready(function() {
+        $('#rootwizard').bootstrapWizard({'tabClass': 'bwizard-steps',
+          'onTabClick': function(tab, navigation, index) {
+            return false;
+          },
+          'onNext': function(tab, navigation, index) {
+          },
+          'onPrevious': function(tab, navigation, index) {
+          }
+        });
+      });
+    },
+    controllerAs: 'clipData'
+    };
+  });
+
+  app.directive("clipInfo", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'view/clip-info-capture.html',
+      controller: function() {
+
+      },
+      controllerAs: 'clipInfo'
+    };
+  });
+  
+})();  
