cd66dc62fc885b7db7e1143a1c15e72cb3d68728
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-01-07 20:08:46 -0800

    1. .gitingore, 2. using real thumbnails for selection

--- js/imctube.js
+++ js/imctube.js
@@ -20,10 +20,22 @@
     return function(input, start) {
       if (!input || !input.length) {
        return;
-     }
+      }
       start = +start;
       return input.slice(start);
     };
   });
+
+  app.filter('range', function() {
+    return function(input, total) {
+      total = parseInt(total);
+
+      for (var i=1; i<=total; i++) {
+        input.push(i);
+      }
+      return input;
+    };
+  });
+
 })();
 
