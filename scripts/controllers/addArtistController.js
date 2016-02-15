b5b0e7735ad8cdc1b415b5cbceded57baffc19f2
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-02-14 21:03:51 -0800

    Ability to update artist information

--- scripts/controllers/addArtistController.js
+++ scripts/controllers/addArtistController.js
@@ -1,16 +1,27 @@
-function AddArtistCtrl($http, $scope) {
-  $scope.artist = {};
+function AddArtistCtrl($http, $scope, toastr) {
+  $scope.submit = function(artist) {
 
-  $scope.submit = function() {
-    $http.post("/imctube/webapi/artists", $scope.artist)
+    if(angular.isDefined(artist.id)) {
+      $http.put("/imctube/webapi/artists/" + artist.id , artist)
       .then(function(data) {
+        toastr.success("Successfully added artist to database!!");
         // Success nothing to do here
       }, function(data) {
+        toastr.error("Failed to add artist to database. Please Re-enter!!");
         console.log("Failure" + data);
       });
-    this.artist = {};
+    } else {
+      $http.post("/imctube/webapi/artists", artist)
+      .then(function(data) {
+        toastr.success("Successfully updated artist info");
+        // Success nothing to do here
+      }, function(data) {
+        toastr.error("Failed to update artist info, Please Re-enter!!");
+        console.log("Failure" + data);
+      });
+    }
   }
 }
 
-AddArtistCtrl.$inject = ['$http', '$scope'];
+AddArtistCtrl.$inject = ['$http', '$scope', 'toastr'];
 angular.module('imctubeApp').controller('AddArtistCtrl', AddArtistCtrl);
