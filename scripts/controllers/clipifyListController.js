71fbbada40f5171d72c27d0d8ab5e9b851359705
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-02-21 13:28:32 -0800

    Option to choose another movie to clipify

--- scripts/controllers/clipifyListController.js
+++ scripts/controllers/clipifyListController.js
@@ -1,10 +1,20 @@
-function ClipifyListCtrl($http, $routeParams, $scope) {
+function ClipifyListCtrl($http, $routeParams, $scope, $window) {
   $scope.movies = [];
 
   $http.get('/imctube/webapi/clipify').success(function(movies) {
     $scope.movies = movies;
   });
+
+  $scope.chooseAnother = function() {
+    $http.post('/imctube/webapi/clipify/chooseAnother')
+      .then(function() {
+        $window.open('#clipify', "_self");
+        // Success, nothing to do here.
+      }, function(error) {
+        console.log("Failed with error " + error);
+      });
+  };
 };
 
-ClipifyListCtrl.$inject = ['$http', '$routeParams', '$scope'];
+ClipifyListCtrl.$inject = ['$http', '$routeParams', '$scope', '$window'];
 angular.module('imctubeApp').controller('ClipifyListCtrl', ClipifyListCtrl);
