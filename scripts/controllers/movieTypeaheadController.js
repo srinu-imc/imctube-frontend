1d9daada9fc1c7f68dfea41f2647f8f540e4748e
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-03-24 19:59:35 -0700

    Removing console.log message

--- scripts/controllers/movieTypeaheadController.js
+++ scripts/controllers/movieTypeaheadController.js
@@ -2,6 +2,5 @@ angular.module('imctubeApp')
   .controller('MovieTypeaheadCtrl', function($scope, $http) {
     $http.get('/imctube/webapi/movies/all').success(function(data) {
       $scope.items = data;
-      console.log($scope.items);
     });
   });
