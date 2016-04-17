5c4dc39afe88aaf137a53577b76ffb57052228e2
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-04-16 20:30:35 -0700

    Showing all clips in review

--- scripts/controllers/reviewClipListController.js
+++ scripts/controllers/reviewClipListController.js
@@ -1,14 +1,9 @@
 function ReviewClipListCtrl($http, $routeParams, $scope, $window) {
   $scope.clips = [];
-  $scope.limit = 30;
 
   $http.get('/imctube/webapi/review/' + $routeParams.movieId + '/clips/').success(function(clips) {
     $scope.clips = clips;
   });
-
-  $scope.expand = function() {
-    $scope.limit = $scope.limit + 30;
-  }
 };
 
 ReviewClipListCtrl.$inject = ['$http', '$routeParams', '$scope', '$window'];
