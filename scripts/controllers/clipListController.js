f461d27db53bdda6ee007695c53ead9518cfea11
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-04-24 13:47:30 -0700

    Skipping clips which doesn't have thumbnails available

--- scripts/controllers/clipListController.js
+++ scripts/controllers/clipListController.js
@@ -1,4 +1,4 @@
-function ClipListCtrl($http, $scope, $routeParams) {
+function ClipListCtrl($http, $scope, $routeParams, $q) {
   $scope.clips = [];
   $scope.page = 0;
   $scope.isLast = false;
@@ -6,8 +6,31 @@ function ClipListCtrl($http, $scope, $routeParams) {
 
   $http.get('/imctube/webapi/clips?page=' + $scope.page).success(function(data) {
     $scope.isLast = !data.hasMoreClips;
-    $scope.clips.push.apply($scope.clips, data.clips);
+    filterClips(data.clips, $scope.clips);
   });
+
+  isImage = function(index, src) {
+      var deffered = $q.defer();
+      var image = new Image();
+      image.onerror = function() {
+        deffered.resolve({"available" : false, "index" : index});
+      };
+      image.onload = function() {
+        deffered.resolve({"available" : true, "index" : index});
+      };
+      image.src = src;
+      return deffered.promise;
+    }
+
+  filterClips = function(clips, output) {
+    for(var i =0; i< clips.length; i++) {
+      isImage(i, clips[i].thumbnails[0]).then(function(result) {
+        if(result.available == true) {
+          output.push(clips[result.index]);
+        }
+      });
+    }
+  }
 
   $scope.loadNext = function() {
     $scope.page = $scope.page + 1;
@@ -20,7 +43,7 @@ function ClipListCtrl($http, $scope, $routeParams) {
 
     $http.get(query).success(function(data) {
       $scope.isLast = !data.hasMoreClips;
-      $scope.clips.push.apply($scope.clips, data.clips);
+      filterClips(data.clips, $scope.clips);
     });
   }
 
@@ -29,7 +52,8 @@ function ClipListCtrl($http, $scope, $routeParams) {
     $scope.searchArtist = angular.copy(artist);
     $http.get('/imctube/webapi/artists/' + artist.id + '/clips?page=' + $scope.page).success(function(data) {
       $scope.isLast = !data.hasMoreClips;
-      $scope.clips = data.clips;
+      $scope.clips = [];
+      filterClips(data.clips, $scope.clips);
     });
   }
 
@@ -39,10 +63,11 @@ function ClipListCtrl($http, $scope, $routeParams) {
     $scope.searchArtist = {};
     $http.get('/imctube/webapi/clips?page=' + $scope.page).success(function(data) {
       $scope.isLast = !data.hasMoreClips;
-      $scope.clips = data.clips;
+      $scope.clips = [];
+      filterClips(data.clips, $scope.clips);
     });
   }
 };
 
-ClipListCtrl.$inject = ['$http', '$scope', '$routeParams'];
+ClipListCtrl.$inject = ['$http', '$scope', '$routeParams', '$q'];
 angular.module('imctubeApp').controller('ClipListCtrl', ClipListCtrl);
